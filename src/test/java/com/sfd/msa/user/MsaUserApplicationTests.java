9359c480bc7137c980440d59e10598b476a64ba3
Bryam David Vega Moreno <>
2023-09-28 21:55:39 -0500

    first commit

---
+++ src/test/java/com/sfd/msa/user/MsaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfd.msa.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
